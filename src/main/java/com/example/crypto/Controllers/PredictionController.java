ed10063adc79be46843406b5db83c6008ca5d4aa
Shazad <shazad.sourcecode@gmail.com>
2021-02-08 14:00:29 +0500

    prediction improvements

--- src/main/java/com/example/crypto/Controllers/PredictionController.java
+++ src/main/java/com/example/crypto/Controllers/PredictionController.java
@@ -19,33 +19,36 @@ public class PredictionController {
     @GetMapping("/{coin}/{days}")
     public ResponseEntity<?> get(@PathVariable("coin") String coin,@PathVariable("days") int days){
 
-        long DAY_IN_MS = 1000 * 60 * 60 * 24;
-        Date date = new Date(System.currentTimeMillis() - (5 * DAY_IN_MS));
-        long from = date.getTime() / 1000;
 
-        //data collection
-        List<CoinHistory> lst = p.getCoinHistory(coin,from).collectList().block();
 
-        double[] x = new double[lst.size()];
-        double[] y = new double[lst.size()];
-        int i = 0;
+        List<Map<String, Object>> response = new ArrayList();
 
-        for(CoinHistory ch:lst){
-            x[i] = i+1;
-            y[i] = ch.getHigh();
-            i++;
-        }
-        LinearRegression o = new LinearRegression(x,y);
+        for(int m = days;m > 0;m--) {
+            long DAY_IN_MS = 1000 * 60 * 60 * 24;
+            Date date = new Date(System.currentTimeMillis() - (m * DAY_IN_MS));
+            long from = date.getTime() / 1000;
 
-        List<Map<String,Object>> response = new ArrayList();
+            //data collection
+            List<CoinHistory> lst = p.getCoinHistory(coin, from).collectList().block();
 
-        for(int j = 0;j < days;j++){
-            Map<String,Object> rsp = new HashMap();
-            Date tstamp = new Date(System.currentTimeMillis() - ((j+1) * DAY_IN_MS));
-            rsp.put("timestamp",tstamp.getTime() / 1000);
-            rsp.put("prediction",o.predict(i));
-            response.add(rsp);
-            i++;
+            double[] x = new double[lst.size()];
+            double[] y = new double[lst.size()];
+            int i = 0;
+
+            for (CoinHistory ch : lst) {
+                x[i] = i + 1;
+                y[i] = ch.getClose();
+                i++;
+            }
+                LinearRegression o = new LinearRegression(x, y);
+
+                Map<String,Object> rsp = new HashMap();
+                Date tstamp = new Date(System.currentTimeMillis() - ((m) * DAY_IN_MS));
+                rsp.put("timestamp", tstamp.getTime() / 1000);
+                rsp.put("prediction", o.predict(i));
+                response.add(rsp);
+                i++;
+
         }
 
 
